ea9922d48b0dedb40e80b49dd68686d19772fb24
ferraijv <ferraioloj@gmail.com>
2023-05-04 18:55:39 -0500

    added check to only cancel orders if no positions for event exist

--- src/kalshi/midday.py
+++ src/kalshi/midday.py
@@ -3,4 +3,6 @@ import datetime
 
 if __name__ == '__main__':
     market_id = shared.create_sp_market_id(run_date=datetime.date.today())
-    shared.cancel_all_orders_for_market(market_id)
+    fulfilled_orders = shared.check_for_fulfilled_orders(market_id)
+    if len(fulfilled_orders) == 0:
+        shared.cancel_all_orders_for_market(market_id)
